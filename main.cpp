a140350ae15637f67f7b4634d71c1d587241d7d1
BlackKvader <black.kvader@gmail.com>
2016-01-20 13:27:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 /*** fast programing version of lattice boltzman ***/
 
+#include <iostream>
 
-#include "read_input_file.hpp"
+// #include "read_input_file.hpp"
+#include "Tsystem.hpp"
 
 using namespace std;
 
 int main(){
+	Tsystem my_system;
 
+	my_system.init();
+	my_system.run();
+	my_system.check();
+	// my_system.close();
+
+	cout << "I'm done!" <<endl;
 	return 0;
 }
-
