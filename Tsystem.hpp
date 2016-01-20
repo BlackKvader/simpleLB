1383875cbfe6d37204a8dc5dc0ba56a1d16c0daa
BlackKvader <black.kvader@gmail.com>
2016-01-20 13:30:30 +0100

    Update Tsystem.hpp

--- Tsystem.hpp
+++ Tsystem.hpp
@@ -1,9 +1,28 @@
 /*** main system class ***/
 
+#ifndef TSYSTEM_HPP
+#define TSYSTEM_HPP
 
 #include <cstring>
 #include <iostream>
 
+#include "Tpoint.hpp"
+
 using namespace std;
 
-class Tsystem;
+class Tsystem{
+public:
+	// Tpoint **point;
+	// OK. I give up. But it was working for while ...
+	Tpoint point[41][1];
+
+	// functions
+	void read_parameter_file(const string parameter_filename); // probably need const *char ...
+	void run();
+	void init();
+	void check();
+	void close();
+	void write(int num);
+};
+
+#endif
